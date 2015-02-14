0b60be5d70001a4921bb9d2079f7fd85c6a8d6c2
Ludovic Cléroux <ludydoo@gmail.com>
2015-02-14 08:56:02 -0500

    Updated financial app Updated foreign key

--- examples/financial/app.js
+++ examples/financial/app.js
@@ -43,35 +43,6 @@ console.log('Accounts : ');
 
 data.accounts.print();
 
-// Adding transactions
-
-data.transactions = new a.Relation([
-    {id : {type : a.Integer, autoIncrement : true}},
-    {accountId : {type : a.Integer}},
-    {amount : {type : a.Float}},
-    {description : {type : a.String}},
-    {date : {type : a.Date}}
-],[
-    [undefined, 0,  -20.00, 'Withdrawal',   new Date()],
-    [undefined, 1,  -10.00, 'Bought Shoes', new Date()],
-    [undefined, 1,   10.00, 'Bought Gifts', new Date()],
-    [undefined, 0,  -10.23, 'New Boobs',    new Date()],
-    [undefined, 1, -100.00, 'New Furs',     new Date()]
-],{
-    pk : 'id',
-    fk : [
-        {
-            columnNames           : 'accountId',
-            referencedColumnNames : 'id',
-            referencedRelation    : data.accounts
-        }
-    ]
-});
-
-
-console.log('Transactions : ');
-
-data.transactions.print();
 
 
 data.transactionTypes = new a.Relation([
@@ -79,7 +50,16 @@ data.transactionTypes = new a.Relation([
     {name : {type : a.String}},
     {parentId : {type : a.Integer}}
 ],[
-   [0, 'Root Category', 0]
+    [0, 'Root', 0],
+    [1, 'Home', 0],
+    [2, 'Transport', 0],
+    [3, 'Sports', 0],
+    [4, 'Entertainment', 0],
+    [5, 'Bills & Utilities', 0],
+    [6, 'Income', 0],
+    [7, 'Uncategorized', 0],
+    [8, 'Clothing', 0],
+    [9, 'Donations and Gifts', 0]
 ],{
     pk : 'id',
     fk : [
@@ -94,3 +74,57 @@ data.transactionTypes = new a.Relation([
 console.log('Transaction Types');
 
 data.transactionTypes.print();
+
+
+// Adding transactions
+
+data.transactions = new a.Relation([
+    {id : {type : a.Integer, autoIncrement : true}},
+    {accountId : {type : a.Integer}},
+    {transactionTypeId : {type : a.Integer}},
+    {amount : {type : a.Float}},
+    {description : {type : a.String}},
+    {date : {type : a.Date}}
+],[
+    [undefined, 0, 7,  -20.00, 'Withdrawal',   new Date()],
+    [undefined, 1, 8,  -10.00, 'Bought Shoes', new Date()],
+    [undefined, 1, 9,   10.00, 'Bought Gifts', new Date()],
+    [undefined, 0, 4,  -10.23, 'New Boobs',    new Date()],
+    [undefined, 1, 4, -100.00, 'New Furs',     new Date()]
+],{
+    pk : 'id',
+    fk : [
+        {
+            columnNames           : 'accountId',
+            referencedColumnNames : 'id',
+            referencedRelation    : data.accounts
+        },
+        {
+            columnNames           : 'transactionTypeId',
+            referencedColumnNames : 'id',
+            referencedRelation    : data.transactionTypes
+        }
+    ]
+});
+
+// data.transactions.pk(['id']);
+
+// data.transactions.pk('id');
+
+// data.transactions.uks().add(['']);
+
+// data.transactions.indexes().add(['id', 'accountId', 'transactionTypeId']);
+
+// data.transactions.indexes().remove(['id', 'accountId', 'transactionTypeId']);
+
+// data.transactions.fks().add({
+//   columnNames : ['accountId'],
+//   referencedRelation : data.accounts,
+//   referencedColumnNames : ['id'],
+//   onDelete : 'cascade',
+//   onUpdate : 'cascade'
+// });
+
+console.log('Transactions : ');
+
+data.transactions.print();
